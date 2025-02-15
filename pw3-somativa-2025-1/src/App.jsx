9f4e9a2b15f4fb3391ee5264078a50140e8a9dfe
profcristianomoraes <cristianocorrea3@gmail.com>
2025-02-15 12:28:18 -0300

    LImpeza dos arquivos iniciais. Conceitos de componentização e criação do primeiro componente (ainda em desenvolvimento).

--- pw3-somativa-2025-1/src/App.jsx
+++ pw3-somativa-2025-1/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import BookCard from './components/BookCard'
+
+import capa_livro from './assets/cavernas_aco.jpg'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        <h1>PW3 - WEBAPP - LIVRARIA</h1>
+
+        <BookCard 
+          titulo='As Cavernas de Aço' 
+          autor='Isaac Azimov'
+          imagem={capa_livro}/>
+
+        {/* <BookCard 
+          titulo='O Sol Desvelado' 
+          autor='Isaac Azimov'
+          imagem='Uma imagem vai aparecer aqui'/>
+
+        <BookCard 
+          titulo='O Fim da Infancia' 
+          autor='Arthur C. Clark'
+          imagem='Uma imagem vai aparecer aqui'/>
+
+        <BookCard 
+          titulo='Neuromancer' 
+          autor='Willian Gibson'
+          imagem='Uma imagem vai aparecer aqui'/> */}
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
