501f29c687f894d2845a9719e84d16bb581ad4cd
profcristianomoraes <cristianocorrea3@gmail.com>
2025-03-06 20:30:01 -0300

    Formulário de cadastro de livro

---
+++ pw3-somativa-2025-1/src/components/form/Select.jsx
@@ -0,0 +1,21 @@
+import style from './Select.module.css'
+
+function Select({text, name, id}) {
+    return(
+        <div className={style.form_control}>
+
+            <label htmlFor={name}>{text}</label>
+
+            <select name={name} id={id}>
+                <option value="">Selecione uma categoria</option>
+                <option value="">Ficçãop cientifica</option>
+                <option value="">Fantasia heroica</option>
+                <option value="">Suspense</option>
+                <option value="">Terror</option>
+            </select>
+
+        </div>
+    )
+}
+
+export default Select;
--- pw3-somativa-2025-1/src/components/pages/CreateBook.jsx
+++ pw3-somativa-2025-1/src/components/pages/CreateBook.jsx
@@ -1,11 +1,53 @@
 import React from "react";
 import style from './CreateBook.module.css'
 
+import Input from "../form/Input";
+import Select from "../form/Select";
+import Button from "../form/Button";
+
 const CreateBook = ()=>{
     return(
-        <section>
-            <h1>CREATE BOOK</h1>
+
+        <section className={style.create_book_container}>
+
+            <h1>CADASTRO DE LIVRO</h1>
+
+            <Input
+                text='Nome do livro'
+                type='text'
+                name='txt_livro'
+                id='txt_livro'
+                placeholder='Digite o nome do livro'
+            />
+
+            <Input
+                text='Autor do livro'
+                type='text'
+                name='txt_autor'
+                id='txt_autor'
+                placeholder='Digite o nome do autor do livro'
+            />
+
+            <Input
+                text='Descrição do livro'
+                type='text'
+                name='txt_descricao'
+                id='txt_descricao'
+                placeholder='Digite a descrição do livro'
+            />
+
+            <Select 
+                name='slc_categoria'
+                id='slc_categoria'
+                text='Categoria do livro'
+            />
+
+            <Button
+                label='CADASTRAR LIVRO'
+            />
+
         </section>
+
     )
 }
 
